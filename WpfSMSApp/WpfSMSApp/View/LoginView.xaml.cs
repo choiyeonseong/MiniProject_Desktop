874f44a489175af14e156d3c4326da6609637b3f
choiyeonseong <am3251@naver.com>
2021-03-25 17:22:48 +0900

    0325 WpfSMSApp 메인메뉴

---
+++ WpfSMSApp/WpfSMSApp/View/LoginView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace WpfSMSApp.View
+{
+    /// <summary>
+    /// LoginView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginView : MetroWindow
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
