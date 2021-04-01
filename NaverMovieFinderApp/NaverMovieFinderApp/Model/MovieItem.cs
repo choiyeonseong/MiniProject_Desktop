e1be4763bdc46b1428320c2f2df835efa5a77cf1
choiyeonseong <am3251@naver.com>
2021-04-01 17:27:49 +0900

    0401 moviefinder

---
+++ NaverMovieFinderApp/NaverMovieFinderApp/Model/MovieItem.cs
@@ -0,0 +1,27 @@
+﻿namespace NaverMovieFinderApp.Model
+{
+    class MovieItem
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Image { get; set; }
+        public string Subtitle { get; set; }
+        public string PubDate { get; set; }
+        public string Director { get; set; }
+        public string Actor { get; set; }
+        public string UserRating { get; set; }
+
+        public MovieItem(string title, string link, string image, string subtitle,
+                         string pubDate, string director, string actor, string userRating)
+        {
+            Title = title;
+            Link = link;
+            Image = image;
+            Subtitle = subtitle;
+            PubDate = pubDate;
+            Director = director;
+            Actor = actor;
+            UserRating = userRating;
+        }
+    }
+}
